98089c088521fbff7e224b1de3d24b5a77b04c5f
Arivazhagan <arivu86@gmail.com>
2012-08-23 05:17:56 +0530

    Testing Pass and Failing
    Using Jasmine Framework and two Chutzpah extensions

--- IntelBingMap/IntelBingMap/test/code.js
+++ IntelBingMap/IntelBingMap/test/code.js
@@ -1,5 +1,20 @@
-﻿var mathLib = {
-    add5: function (z) {
-        return z + 5;
+﻿var stringLib = {
+    vowels: function (a) {
+        count = 0;
+        for (var i = 0; i < a.length; i++) {
+            if ("aeiou".indexOf(a[i]) > -1) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
+
+var mathLib = {
+    add5: function (a) {
+        return a + 5;
+    },
+    mult5: function (a) {
+        return a * 5;
     }
 }
