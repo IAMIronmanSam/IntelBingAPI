6e853f01404d29a02572745a1385f11aa90e5941
Arivazhagan <arivu86@gmail.com>
2012-08-28 22:48:35 +0530

    Background Image & Navigation Links
    Testing

--- IntelBingMap/IntelBingMap/js/Locate.js
+++ IntelBingMap/IntelBingMap/js/Locate.js
@@ -1,8 +1,7 @@
 ﻿(function () {
     "use strict";
 
-    //var app = WinJS.Application;
-    //var activation = Windows.ApplicationModel.Activation;
+    
     var nav = WinJS.Navigation;
     WinJS.strictProcessing();
     Microsoft.Maps.loadModule('Microsoft.Maps.Map', { callback: initMap });
@@ -40,50 +39,5 @@
             map.entities.push(pushpin);
             map.setView({ center: new Microsoft.Maps.Location(latitude, longitude) });
         })
-
-
-
     }
-
-
-    //app.addEventListener("activated", function (args) {
-    //    if (args.detail.kind === activation.ActivationKind.launch) {
-    //        if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
-    //            // TODO: This application has been newly launched. Initialize
-    //            // your application here.
-    //        } else {
-    //            // TODO: This application has been reactivated from suspension.
-    //            // Restore application state here.
-    //        }
-
-    //        if (app.sessionState.history) {
-    //            nav.history = app.sessionState.history;
-    //        }
-    //        args.setPromise(WinJS.UI.processAll().then(function () {
-    //            if (nav.location) {
-    //                nav.history.current.initialPlaceholder = true;
-    //                return nav.navigate(nav.location, nav.state);
-    //            } else {
-    //                return nav.navigate(Application.navigator.input);
-    //            }
-    //        }));
-    //    }
-    //});
-
-    
-    //WinJS.UI.Pages.define("/Pages/Locate.html", {
-    //    ready: function (element, options) {
-    //        document.getElementById("Gointo").addEventListener("click", navil, false);
-    //    }
-    //});
-
-     
-
-    //function navil() {
-    //    console.log("Able to Navogate");
-    //    nav.navigate('/Pages/input.html');
-    //    console.log("k k");
-
-    //}
-
 })();
